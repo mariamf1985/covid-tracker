aba590d15bdd0693a8b44c10cfa09ba156f57372
NathaRuiz <nruizm98@gmail.com>
2023-12-21 23:50:34 +0100

    create card box

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import VirusCardBackground from './components/Cards/VirusCardBackground'
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className=' bg-blue-800 w-full h-80'>
+      <VirusCardBackground/>
+    </div>
   )
 }
 
--- src/components/Cards/VirusCardBackground.jsx
+++ src/components/Cards/VirusCardBackground.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const VirusCardBackground = () => {
   return (
-    <div>VirusCardBackground</div>
+    <div className=' bg-white w-48 h-28'>
+        <div className='w-10 h-28'>
+            <img src="../../assets/images/icons/covid-blue.svg" alt="covid" />
+        </div>
+        <div className=''>
+            <h5>Total Cases</h5>
+            <h3>5871977</h3>
+        </div>
+    </div>
   )
 }
 
