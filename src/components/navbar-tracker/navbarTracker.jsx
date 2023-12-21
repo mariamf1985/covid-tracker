829711c1c279cb919352b14919dc4494ee91f29f
NathaRuiz <nruizm98@gmail.com>
2023-12-21 20:58:40 +0100

    implement hook in navbar for change styles

--- src/components/navbar-tracker/navbarTracker.jsx
+++ src/components/navbar-tracker/navbarTracker.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { RiPieChartLine } from "react-icons/ri";
 import { TiThMenuOutline,TiThLargeOutline } from "react-icons/ti";
 import { TfiLayoutTab,TfiLayersAlt,TfiStatsUp } from "react-icons/tfi";
@@ -7,26 +8,39 @@ import { AiOutlineGlobal } from "react-icons/ai";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { IoLogoFlickr } from "react-icons/io5";
 
+
 const NavbarTracker = () => {
+  
+  const [activeNavItem, setActiveNavItem] = useState(null);
+
+  const handleNavItemClick = (navItem) => {
+    setActiveNavItem(navItem);
+  };
+
+  const getNavItemStyle = (navItem) => {
+    const baseStyle = "text-secondarybg hover:bg-secondarybg hover:text-primarybackground w-full p-4 rounded";
+    return activeNavItem === navItem ? 'bg-secondarybg text-primarybackground w-full p-4 rounded' : baseStyle;
+  };
+
   return (
-    <nav className="flex flex-col  items-center h-screen w-20  bg-primarybackground">
-       <ul className="h-full flex flex-col items-center justify-around">
+    <nav className="flex flex-col items-center h-screen w-20 bg-primarybackground">
+      <ul className="h-full flex flex-col items-center justify-around">
         <li>
           <Link to="/">
-          <img src="../src/assets/images/coronavirus.png"  className="animate-spin-slow w-12 mt-3 mb-3"></img>
+            <img src="../src/assets/images/coronavirus.png" className="animate-spin-slow w-12 mt-3 mb-3" alt="Coronavirus" />
           </Link>
         </li>
-        <li className="text-secondarybg hover:bg-secondarybg hover:text-primarybackground w-full p-4 rounded">
+        <li onClick={() => handleNavItemClick("tracker-1")} className={getNavItemStyle("tracker-1")} >
           <Link to="/tracker-1">
-            <RiPieChartLine size={27}/>
+            <RiPieChartLine size={27} />
           </Link>
         </li>
-        <li className="text-secondarybg hover:bg-secondarybg hover:text-primarybackground w-full p-4 rounded">
+        <li onClick={() => handleNavItemClick("tracker-2")} className={getNavItemStyle("tracker-2")} >
           <Link to="/tracker-2">
             <TiThMenuOutline size={27}/>
           </Link>
         </li>
-        <li className="text-secondarybg hover:bg-secondarybg hover:text-primarybackground w-full p-4 rounded">
+        <li onClick={() => handleNavItemClick("tracker-3")} className={getNavItemStyle("tracker-3")} >
           <Link to="/tracker-3">
             <TiThLargeOutline size={27}/>
           </Link>
