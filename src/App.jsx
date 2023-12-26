76b782bad012fd524a2ab737b51c513f63e32bc6
NathaRuiz <nruizm98@gmail.com>
2023-12-26 13:42:12 +0100

    insert props for the info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import VirusCardBackground from './components/Cards/VirusCardBackground'
+import covid from '../src/assets/images/icons/covid-defult.svg';
+import covidR from '../src/assets/images/icons/covid-red.svg';
 
 function App() {
   
   return (
-    <div className=' bg-blue-800 w-full h-80'>
-      <VirusCardBackground/>
+    <div className=' bg-blue-800 w-full h-80 p-5'>
+      <VirusCardBackground title="Total Cases" value="1234567" valueColor="#3639AE" image={covid} />
+      <VirusCardBackground title="Total Deaths" value="1234567" valueColor="red" image={covidR} />
     </div>
   )
 }
--- src/components/Cards/VirusCardBackground.jsx
+++ src/components/Cards/VirusCardBackground.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import covid from '../../assets/images/icons/covid-blue.svg';
 
-const VirusCardBackground = () => {
+const VirusCardBackground = ({ title, value, image, valueColor }) => {
+  const valueStyle = { color: valueColor };
   return (
-    <div className='bg-white w-52 h-24 relative overflow-hidden rounded-sm shadow-md '>
+    <div className='bg-white w-52 h-24 relative overflow-hidden rounded-sm shadow-md m-3.5'>
 
-      <div className='absolute bottom-2 -right-6 w-28 h-28 opacity-25'>
-        <img src={covid} alt='covid' className='w-full h-full object-cover' />
+      <div className='absolute bottom-2 -right-6 w-28 h-28 opacity-10'>
+        <img src={image} alt='covid' className='w-full h-full object-cover' />
       </div>
 
       <div className='relative z-10 w-full h-full flex flex-col justify-between items-left'>
-        <h5 className='text-sm font-semibold text-gray-800 w-full border-b-2 pt-2 pl-2.5 pb-3'>Total Cases</h5>
-        <h3 className='text-3xl font-bold text-secondarybg p-2.5'>5871977</h3>
+        <h5 className='text-sm font-semibold text-gray-800 w-full border-b-2 pt-2 pl-2.5 pb-3'>{title}</h5>
+        <h3  style={valueStyle} className='text-3xl font-bold p-2.5'>{value}</h3>
       </div>
 
     </div>
