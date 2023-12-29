55c014d3c46ad53b3126170b91ef444a3eaf5ea1
NathaRuiz <nruizm98@gmail.com>
2023-12-29 13:12:42 +0100

    delete import link

--- src/components/navbar-tracker/navbarTracker.jsx
+++ src/components/navbar-tracker/navbarTracker.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { RiPieChartLine } from "react-icons/ri";
 import { TiThMenuOutline,TiThLargeOutline } from "react-icons/ti";
