84d576aad48864367e48b29ed5639cbc95e93158
NathaRuiz <nruizm98@gmail.com>
2024-01-07 15:18:38 +0100

    created and implement VirusCardIcon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primarybackground' : '#F7F8FC', 
+        'secondarybg' : '#3639AE',
+        'primaryLetterColor' : '#122672',
+        'secondaryLetterColor' :'#2C6DFF',
+        'tertiaryLetterColor' : '#82C519',
+        'colorCard1' : '#FFF2F2',
+        'colorCard2' : '#FFF9F1',
+        'colorCard3' : '#EDFFEC',
+        'colorCard4' : '#F4FCFF',
+        'bisque' : '#FFE4C4',
+        'quarteryLetterColor' : '#FF6A07',
+        'quinaryLetterColor' : '#B70202',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
