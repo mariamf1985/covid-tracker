c7498e0595b4435f0c38f4d14cbcd3e3920cdbcf
mariamf1985 <mariamf1985@gmail.com>
2024-01-08 21:25:22 +0100

    props added on CountryButton.jsx

--- src/components/buttons/CountryButton.jsx
+++ src/components/buttons/CountryButton.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const CountryButton = () => {
+const CountryButton = ({ image, country, value }) => {
     return (
         <button class="flex items-center border text-primaryLetterColor px-3 py-2">
             <div class="rounded-full overflow-hidden mr-2">
       
-      <img src="" class="w-8 h-8"/>
+      <img src="{image}" alt="flag" class="w-8 h-8"/>
     </div>
     <div class="flex flex-col justify-start">
-      <span class="text-left text-sm">country</span>
-      <span class="text-left font-semibold text-lg">0123456789</span>
+      <span class="text-left text-sm">{country}</span>
+      <span class="text-left font-semibold text-lg">{value}</span>
     </div>
         </button>
     )
