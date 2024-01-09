70ff99a386f1d01c386c1a66110c0c7b6aa3c78f
Marta Parra <149329102+Macata47@users.noreply.github.com>
2024-01-09 11:59:47 +0100

    counterCard props implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import { CounterCard } from './components/Cards/CounterCard'
 
+
+
 function App() {
  
   return (
     <>
-      <CounterCard/>
+      <CounterCard bgColor = {"#FFF2F2"}  tittle={"Total Cases"} numColor={'#122672'} dayNum={"1234"} totalValue={"47859"}/>
+      <CounterCard bgColor = {'#FFF9F1'}  tittle={"Active Cases"} numColor={'#FF6A07'} totalValue={"47859"}/>
+      <CounterCard bgColor = {'#EDFFEC'}  tittle={"Recovered Cases"} numColor={'#B70202'} dayNum={"1234"} totalValue={"47859"}/>
+      <CounterCard bgColor = {'#F4FCFF'}  tittle={"Deaths Cases"} numColor={'#2C6DFF'} dayNum={"1234"} totalValue={"47859"}/>
     </>
   )
 }
---
+++ src/services/UseFetch.jsx
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react';
+
+const UseFetch = (url) => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        const getData = async (url) => {
+            let response = await fetch(url)
+            console.log(response)
+            let responseJson = await response.json()
+            setData(responseJson)
+        }
+        getData(url)
+    }, [url]);
+    return {data}
+}
+
+export default UseFetch
