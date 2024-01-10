3a4d9867c50e46652ba3291902aed003a99425e1
NathaRuiz <nruizm98@gmail.com>
2024-01-10 01:16:03 +0100

    update responsive design in navbarTracker and Tracker-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import NavbarTracker from './components/navbar-tracker/navbarTracker'
 import Tracker1 from './pages/tracker-1'
 
 import React from 'react'
 function App() {
 
   return (
-    <>
+    <div>
+      <NavbarTracker/>
       <Tracker1/>    
-    </>
+    </div>
   )
 }
 
--- src/pages/tracker-1.jsx
+++ src/pages/tracker-1.jsx
@@ -23,14 +23,14 @@ const Tracker1 = () => {
         }
     }
   return (
-    <section className='bg-primarybackground float-right sm:p-[30px] xl:w-[65%] md:w-[90%] min-[375px]:w-[90%] min-[375px]:p-[10px] h-full'>
+    <section className='bg-primarybackground float-right sm:p-[30px] xl:w-[70%] lg:w-[92.5%] md:w-[90.5%] min-[375px]:w-[81.5%] min-[375px]:p-[10px] max-[375px]:w-[79%] h-screen'>
       <div className='w-full md:flex justify-between items-center border-b-2 pb-3  max-[600px]:flex max-[600px]:flex-col'>
         {!loading  ? <CountrySelect countries={data} chooseCountry={updateSelectedCountry}/> : <p className='font-bold'>Loading...</p>}
         <div className='font-medium'>Update: January 31, 2022</div>
       </div>
       <div className='w-full lg:flex pt-3.5 pb-7 max-[600px]:flex max-[600px]:flex-col'>
         <VirusCardsBg selectCountry={selectedCountry}/>
-        <img src={map} className='mt-3.5 lg:w-[43%]'></img>
+        <img src={map} className='mt-3.5 lg:w-[43%] min-[1300px]:w-[48%]'></img>
       </div>
       <div>
       {!globalCases.loading ? <VirusCardsIcons allCases={globalCases.data}/> : <VirusCardsIcons allCases={{}}/>}
