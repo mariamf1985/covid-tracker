c1e33bc25463cd8130df6e3741473e33509e2cba
NathaRuiz <nruizm98@gmail.com>
2024-01-11 16:49:19 +0100

    add visual effects in the virusCardBackground

--- src/components/Cards/VirusCardBackground.jsx
+++ src/components/Cards/VirusCardBackground.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './VirusCardBackground.css'
 
 const VirusCardBackground = ({ title, value = 0, image, valueColor }) => {
   const valueStyle = { color: valueColor };
   return (
-    <div className='bg-white w-52 h-24  max-[450px]:w-[140px] max-[450px]:h-[85px] max-[600px]:w-44  relative overflow-hidden rounded-sm shadow-md '>
+    <div className='bg-white w-52 h-24  max-[450px]:w-[140px] max-[450px]:h-[85px] max-[600px]:w-44  relative overflow-hidden rounded-sm shadow-md transition-transform duration-500 ease-in-out transform hover:-translate-y-2 virusCardBgBox'>
 
-      <div className='absolute bottom-2 -right-6 w-28 h-28 opacity-10 max-[600px]:bottom-0 max-[450px]:w-24 max-[450px]:h-24'>
+      <div className='absolute bottom-2 -right-6 w-28 h-28 opacity-10 max-[600px]:bottom-0 max-[450px]:w-24 max-[450px]:h-24 virusImage'>
         <img src={image} alt='covid' className='w-full h-full object-cover ' />
       </div>
 
