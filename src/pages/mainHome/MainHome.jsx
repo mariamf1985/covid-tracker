f6c6ee6b09323a4d5155e521af394007c5eb65e9
NathaRuiz <nruizm98@gmail.com>
2024-01-12 20:56:35 +0100

    arrange styles in mainHome.css

--- src/pages/mainHome/MainHome.jsx
+++ src/pages/mainHome/MainHome.jsx
@@ -10,17 +10,17 @@ const MainHome = () => {
   };
 
   return (
-    <div className="main-home">
+    <div className="main-home font-[poppins]">
       <nav className="nav">
         <div className="nav-content">
           <div className="logo">
             <img src="./src/assets/images/logo.png" alt="Logo" />
           </div>
           <div className="nav-links">
-              <a href="#">Home</a>
+              <a href="#">Home <i className="fa fa-caret-down"></i></a>
               <a href="#">Prevention</a>
               <a href="#">Quarantine</a>
-              <a href="#">Pages</a>
+              <a href="#">Pages <i className="fa fa-caret-down"></i></a>
               <a href="#">About</a>
               <a href="#">Help</a>
               <button className="tracker"  onClick={handleTrackerButtonClick}>Tracker</button> {}
@@ -32,7 +32,7 @@ const MainHome = () => {
           <h1>Download the covid app for get latest update</h1>
           <p>Human coronaviuses are common anda are typically associated with mild illnesses, similar to the common cold.
           The Corona Virus COVID-19 is affecting 210 countries & territories arround the world and 2 international conveyance</p>
-          <button className="protect-btn">How to protect</button>
+          <button className="protect-btn">HOW TO PROTECT</button>
         </div>
         <div className="image-content">
         <img src="./src/assets/images/phones.png"/>
