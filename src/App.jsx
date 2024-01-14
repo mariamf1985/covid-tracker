806490e55b6542652aa67f71580c8002cc55b74c
mariamf1985 <mariamf1985@gmail.com>
2024-01-14 20:02:40 +0100

    app.css deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 
 import React from 'react'
-import './App.css'
-import NavbarTracker from './components/navbar-tracker/navbarTracker'
-import Tracker0 from './pages/tracker0/Tracker0'
 import Router from './configurations/Router'
 
 
@@ -10,8 +7,6 @@ function App() {
 
   return (
     <>
-    {/* <NavbarTracker/>
-    <Tracker0></Tracker0> */}
     <Router></Router>
     </>
   )
